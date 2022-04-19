7de1d0c3b16a17a9e18e1ee8ba61c44f99c01d3b
Michael Mathu <mathumichael17@gmail.com>
2022-04-19 05:55:43 +0300

    created struct

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using StructNameSpace;
+
+namespace Program
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            TimeStruct time = new TimeStruct ();
+            time.Seconds = 10;
+
+            Console.WriteLine (time.Seconds);
+        }
+
+
+    }
+}
---
+++ TimeStruct.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructNameSpace
+{
+        struct TimeStruct
+        {   
+            private int seconds;
+
+            public int Seconds
+            {
+                get { return seconds; }
+                set { seconds = value; }
+            }
+
+            public int CalculateMinutes()
+            {
+                return seconds / 60;
+            }
+        }
+    
+}
